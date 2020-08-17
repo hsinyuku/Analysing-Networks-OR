3e2318f6b6a7ae0d2d71777fd4e09a73cbc5e2f0
Chan Le <93.chan.le@gmail.com>
2020-08-17 17:06:22 +0200

    distances of batches

--- Project/Power Set & Check Weight.py
+++ Project/Power Set & Check Weight.py
@@ -9,49 +9,73 @@ Created on Tue Aug 11 13:54:23 2020
 import xml.etree.ElementTree as ET
 import pandas as pd
 import copy
+import numpy as np
 
-#%% Extract the item descriptions from XML file to a dictionary
+#%% CONTROL KNOBS
+# Specify the number of orders we receive (either 10 or 20)
+orderAmount = 10
+
+# Specify the number of items in the warehouse (either 24 or 360)
+itemAmount = 24
+
+#%% CREATE ITEM LIST --> object itemList
+# This code block extracts the item descriptions from XML file to a dictionary,
+# put them in a list, and removes all surplus information.
+
+# Parse XML file to tree element
+tree = ET.parse("data/sku" + str(itemAmount) + "/orders_" + str(orderAmount) +
+                "_mean_5_sku_" + str(itemAmount) + ".xml")
+
+# Get the root node
+root = tree.getroot()
+
+# Create empty dictionary
+itemList = list()
+
+# Loop through child node "ItemDescription" and append values to itemList
+count = 0
+for itemDes in root.iter("ItemDescription"):
+    temp = itemDes.attrib
+    itemList.append(temp)
+    count = count + 1  
+
+# From the itemList, keep only ID and Weight
+for i in itemList:
+    i["itemID"] = int(i.pop("ID"))
+    i["Weight"] = float(i.pop("Weight"))
+    i.pop("Color",None)
+    i.pop("Letter",None)
+    i.pop("Type",None)
+
+# Print final itemList:
+    repr(itemList)
     
-    # Parse XML file to tree element
-    tree = ET.parse("data/sku24/orders_20_mean_5_sku_24.xml")
-    
-    # Get the root node
-    root = tree.getroot()
-
-    # Create empty dictionary
-    itemList = list()
-
-    # Loop through child node "ItemDescription" and append values to itemList
-    count = 0
-    for itemDes in root.iter("ItemDescription"):
-        temp = itemDes.attrib
-        itemList.append(temp)
-        count = count + 1
-
-    print(itemList)    
-
 #%% Extract the order descriptions from XML file to a dictionary
-    orderList = list()
+orderList = list()
    
-    for i in range(len(root[2])):
-        orderList.append(list())
-        for j in range(len(root[2][i][0])):
-            orderList[i].append(root[2][i][0][j].attrib)
-            
-#%% Fill the weight information of each item to the orderList
-    for i in range(len(orderList)):
-        for j in range(len(orderList[i])):
-            itemID = orderList[i][j]["ItemDescriptionID"]
-            orderList[i][j]["Weight"] = float(itemList[int(itemID)]["Weight"])
-         
-            
+for i in range(len(root[2])):
+    orderList.append(list())
+    for j in range(len(root[2][i][0])):
+        orderList[i].append(root[2][i][0][j].attrib)
+
+# Rename key itemID
+for order in orderList:
+    for item in order:
+        item["itemID"] = int(item.pop("ItemDescriptionID"))
+        item["Count"] = int(item.pop("Count"))
+           
+# Fill the weight information of each item to the orderList
+for i in range(len(orderList)):
+    for j in range(len(orderList[i])):
+        itemID = orderList[i][j]["itemID"]
+        orderList[i][j]["Weight"] = float(itemList[int(itemID)]["Weight"])
+
 #%% Calculate total weight of each order
     orderWeight = list()
     
     for i in range(len(orderList)):
         orderWeight.append(sum(item['Weight'] for item in orderList[i]))
-        
-        
+
 #%% Function: Calculate total weight of a potential batch
     def FUNC_batchWeight(batch, orderWeight):
         sum = 0
@@ -62,42 +86,117 @@ import copy
 #%% Calculate feasible batches from the orders, only accept batches which are 
 # within the cobot's capacity
    
-orderSet = list(range(0,20))
+orderSet = list(range(0,orderAmount))
 feasibleBatch = list()
 feasibleBatch.append([orderSet.pop(0)])
 feasibleBatchWithWeight = list()
+
+for i in range(len(orderSet)):
+
+    feasibleBatchCopy = copy.deepcopy(feasibleBatch)    
+
+    update = [batch + [orderSet[i]] for batch in feasibleBatchCopy]
+
+    feasibleBatchCopy = copy.deepcopy(update)
+    feasibleBatchCopy.append([orderSet[i]])
+    
+    # Check for cobot capacity fullfilment
+    feasibleBatchCheckWeight = list()
+    
+    for batch in feasibleBatchCopy:
+        weight = round(FUNC_batchWeight(batch, orderWeight),2)
+        print("Potential batch is: " + str(batch) + ", Weight = " + str(weight))
+        if (weight < 18):
+            print("Within capacity. Feasible batch.")
+            feasibleBatchCheckWeight.append(batch)
+            feasibleBatchWithWeight.append({"Batch" : batch, "Weight" : weight})
+        else:
+            print("Cobot capacity exceeded. Remove from list.")
+        print("---")
+    feasibleBatch = feasibleBatch + feasibleBatchCheckWeight
+      
+#%%
+feasibleBatchWeight = list()
+for batch in feasibleBatch:
+    if (FUNC_batchWeight(batch, orderWeight) < 10):
+        feasibleBatchWeight.append(batch)
+
+#%% 
+itemDict = {}
+for i in itemList:
+    itemDict.append(i)
+        
+#%% Navigate through the itemList and print Weight
+for item in itemList:
+    if item["itemID"] == "5":
+        print(item["Weight"])
+        
+                
+#%% From orderList, construct itemsInOrderList
+itemsInOrderList = []
+for i in range(orderAmount):
+    itemsInOrder = []
+    for order in orderList[i]:
+        if (order["itemID"] in itemsInOrder) == False:
+            itemsInOrder.append(order["itemID"])
+    itemsInOrderList.append(itemsInOrder)
+    
+#%% From itemsinOrderList and feasibleBatch, contruct itemsinBatch
+itemsInBatch = []
+for batch in feasibleBatch:
+    print("Batch" + str(batch))
+    itemsInOneBatch = []
+    for i in range(len(batch)):
+        order = batch[i]
+        items = itemsInOrderList[order]
+        itemsInOneBatch = list(set(items) | set(itemsInOneBatch))
+        print("itemsInBatch" + str(itemsInOneBatch))
+    itemsInBatch.append(itemsInOneBatch)
+    
+#%% Create a batchList 
+batchList = []
+for i in range(len(feasibleBatch)):    
+    batch = {"batchID": i, "ordersInBatch":feasibleBatch[i], "itemsInBatch": itemsInBatch[i]}
+    batchList.append(batch)
     
 #%%
-    for i in range(len(orderSet)):
+distances = _demo.distance_ij
 
-        feasibleBatchCopy = copy.deepcopy(feasibleBatch)    
-
-        update = [batch + [orderSet[i]] for batch in feasibleBatchCopy]
-
-        feasibleBatchCopy = copy.deepcopy(update)
-        feasibleBatchCopy.append([orderSet[i]])
-        
-        # Check for cobot capacity fullfilment
-        feasibleBatchCheckWeight = list()
-        
-        for batch in feasibleBatchCopy:
-            weight = round(FUNC_batchWeight(batch, orderWeight),2)
-            print("Potential batch is: " + str(batch) + ", Weight = " + str(weight))
-            if (weight < 18):
-                print("Within capacity. Feasible batch.")
-                feasibleBatchCheckWeight.append(batch)
-                feasibleBatchWithWeight.append({"Batch" : batch, "Weight" : weight})
-            else:
-                print("Cobot capacity exceeded. Remove from list.")
-            print("---")
-        feasibleBatch = feasibleBatch + feasibleBatchCheckWeight
-       
-        
+for i in range(24):
+    print(distances.get(('OutD0',str(i))))
+    
+#%% CREATE DISTANCE MATRICES
+    # Pull calculated distances out of Xie's code and reshape it into a distance
+    # matrix. Each dimension is (amount of items + 2 packing stations)
+    distMat = np.array(list(distances.values())).reshape(itemAmount + 2, itemAmount + 2)
+    # Convert to pandas DataFrame
+    distMat = pd.DataFrame(distMat)
+    # Add column names
+    distMat.columns = ['OutD0', 'OutD1'] + [i for i in range(0,24)]
+    # Add an indexing column FromPod
+    distMat.insert(0, "FromPod", distMat.columns)
+    distMat = distMat.set_index('FromPod')
+    
+    # Extract distances from pods back to the packing stations. This is the last
+    # step in calculating the route.
+    distToStation0 = distMat[["OutD0"]]
+    distToStation1 = distMat[["OutD1"]]
+    distMat = distMat.drop(["OutD0","OutD1"], axis = 1)
+    
+#%% 
+    currentBatch = itemsInBatch[2]
+    route = ["OutD0"] 
+    totalDist = 0
+    
 #%%
-        FUNC_batchWeight(batch, orderWeight)
-        
-#%%
-        feasibleBatchWeight = list()
-        for batch in feasibleBatch:
-            if (FUNC_batchWeight(batch, orderWeight) < 10):
-                feasibleBatchWeight.append(batch)
+    for i in range(len(currentBatch)-1):
+        dist = distMat.loc[route[0],itemsInBatch[2]].to_frame()
+        print(dist)
+        nextNode = dist.idxmin()
+        print(nextNode)
+        distNode = dist.min()
+        print(distNode)
+        route.append(int(nextNode))   
+        # totalDist += distNode
+        # currentBatch.remove(nextNode)
+    # route = route + currentBatch
