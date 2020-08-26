ce9246f3084c13cc7e00654d0f4fc63fe6f8e9c2
InazeaAnazasi <inazea@posteo.org>
2020-08-26 21:04:12 +0300

    wrote comments
    - function to assign orders to batches
    - function to (partly) break a given solution
    - functio to repair a given solution

--- Project/implementation/Task1.py
+++ Project/implementation/Task1.py
@@ -12,13 +12,12 @@ orderAmount = 10
 # Specify the number of items in the warehouse (either 24 or 360)
 itemAmount = 24
 
+# Task 2.1
 #%%
 # From all orders, assign them to the optimal packing station:
 # I use option "lite" here to get two list of orders for two packing station, as
 # input for the next function. To see information of the orders, use option "full"
-station = F_assignOrderToStation(10, "lite")
-
-
+station = F_assignOrderToStation(orderAmount, "lite")
 
 # For each station, create feasible batches from the orders, which also includes
 # the sequence of pods visited for the cobot, as well as the minimum distance
