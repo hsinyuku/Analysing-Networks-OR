e2abf6e18df9934d5773b5bf18b680a6850fa011
Chan Le <93.chan.le@gmail.com>
2020-08-31 21:41:37 +0200

    Solution dedicated part 1

---
+++ Project/implementation/XML SAApy.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 31 15:25:01 2020
+
+@author: 93cha
+"""
+
+
+
+botAmount = 2
+
+
+ 
+#%%
+    
+     for i in batch["routeInBatch"].index():
+         print(i)
